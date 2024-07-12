91d798fd9eb6dacb1fe72a174f62f1d6d132564d
Sudharsan917 <sudharsanyadav917@gmail.com>
2024-07-12 15:35:13 +0530

    2nd yy

--- Java-Project-1/src/main/java/com/sriniit/App.java
+++ Java-Project-1/src/main/java/com/sriniit/App.java
@@ -9,7 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+<<<<<<< Updated upstream
        System.out.println( "Hello World3!" );
+=======
+        System.out.println( "yyyyyyyyyyyyy" );
+
+>>>>>>> Stashed changes
 
     }
 }
